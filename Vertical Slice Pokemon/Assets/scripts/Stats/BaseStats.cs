a9e5b42d590a791acd70cfcbf5dafc04fec6e0e6
Sammy <frnkverdam@gmail.com>
2025-11-19 09:53:48 +0100

    BaseStats

---
+++ Vertical Slice Pokemon/Assets/scripts/Stats/BaseStats.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[System.Serializable]
+public class BaseStats : MonoBehaviour
+{
+    public int baseHealth;
+    public int baseAttack;
+    public int baseDefense;
+    public int baseSpAttack;
+    public int baseSpDefense;
+    public int baseSpeed;
+}
