29bb79532c0b34ed28ef8209b2e1a504c0a0b1bf
38684 <38684@ma-web.nl>
2026-01-20 10:16:06 +0100

    UI Buttons

--- Vertical Slice Pokemon/Assets/Scripts/EnemyAi.cs
+++ Vertical Slice Pokemon/Assets/Scripts/EnemyAi.cs
@@ -1,20 +1,20 @@
 
-using UnityEngine;
-using static TurnOrder;
+//using UnityEngine;
+//using static TurnOrder;
 
-public class EnemyAi : MonoBehaviour
-{
-    PokemonActions pokemonActions;
+//public class EnemyAi : MonoBehaviour
+//{
+//    PokemonActions pokemonActions;
 
-    private void Start()
-    {
-        OnTurnOrder += EnemyTurn;
-        pokemonActions = GetComponent<PokemonActions>();
-    }
+//    private void Start()
+//    {
+//        OnTurnOrder += EnemyTurn;
+//        pokemonActions = GetComponent<PokemonActions>();
+//    }
 
-    private void EnemyTurn(DataStructures.BattleState state)
-    {
-        if (state == DataStructures.BattleState.EnemyTurn)
-            pokemonActions.UseMove(Random.Range(0, 5));
-    }
-}
+//    private void EnemyTurn(DataStructures.BattleState state)
+//    {
+//        if (state == DataStructures.BattleState.EnemyTurn)
+//            pokemonActions.UseMove(Random.Range(0, 5));
+//    }
+//}
--- Vertical Slice Pokemon/Assets/Scripts/turnorder/turnorder.cs
+++ Vertical Slice Pokemon/Assets/Scripts/turnorder/turnorder.cs
@@ -16,39 +16,26 @@ public class TurnOrder : MonoBehaviour
     private void Start()
     {
         state = battleState.start;
-        Debug.Log("I WANNA BE, THE VERY BEST :>");
     }
 
 
     public void PlayerTurn()
     {
         state = battleState.playerturn;
-        Debug.Log("GO MY SLAVE :>");
     }
 
     public void EnemyTurn()
     {
         state = battleState.enemyturn;
-        Debug.Log("NOOOOO MY ENEMY :>");
     }
 
     public void Won()
     {
         state = battleState.won;
-        Debug.Log("you won :>");
     }
 
     public void Lost()
     {
         state = battleState.lost;
-        Debug.Log("you lost :>");
     }
-
-
 }
-
-
-
-
-
-
